b18c80d1050a70aa32353ee0d1326e2e91b8fdee
Omer faruk Balaban <obalaban@k1m41s06.42istanbul.com.tr>
2022-06-30 15:21:56 +0300

    libft

---
+++ libft/ft_atoi.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obalaban <obalaban@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/30 13:43:44 by obalaban          #+#    #+#             */
+/*   Updated: 2022/06/30 15:19:57 by obalaban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int	check_space(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n')
+		return (1);
+	if (c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	return (0);
+}
+
+int	jump_space(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (check_space(str[index]) == 1)
+	{
+		index++;
+	}
+	return (index);
+}
+
+int	ft_atoi(const char *str)
+{
+	int				index;
+	int				k;
+	unsigned long	number;
+
+	index = 0;
+	number = 0;
+	k = 1;
+	index = jump_space((char *)str);
+	if (str[index] == '-')
+		k = -1;
+	if (str[index] == '-' || str[index] == '+')
+		index++;
+	while (str[index] >= '0' && str[index] <= '9')
+	{
+		number = number * 10 + (str[index] - '0');
+		index ++;
+	}
+	if (number * k > 9223372036854775807 && k == 1)
+		return (-1);
+	else if (number * k > 9223372036854775808 && k == -1)
+		return (0);
+	return (number * k);
+}
+
+// int	main(void)
+// {
+// 	char	*c = "    -9223372036854775808";
+
+// 	printf("%i\n", ft_atoi(c));
+// 	printf("%i\n", atoi(c));
+// }
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,11 +6,32 @@
 /*   By: obalaban <obalaban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 19:31:55 by obalaban          #+#    #+#             */
-/*   Updated: 2022/06/29 19:33:57 by obalaban         ###   ########.fr       */
+/*   Updated: 2022/06/30 13:20:32 by obalaban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	
+	size_t	i;
+	size_t	x;
+
+	i = 0;
+	x = 0;
+	if (!needle[i])
+		return ((char *)haystack);
+	while (haystack[i])
+	{
+		while (haystack[i + x] == needle[x] && needle[x] && (i + x) < len)
+		{
+			x++;
+		}
+		if (needle[x] == '\0')
+			return ((char *)&haystack[i]);
+		x = 0;
+		i++;
+	}
+	return (0);
 }
