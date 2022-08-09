ca6ddb8385a331cc234b6b15d00001fb4c14ab81
Omer faruk Balaban <obalaban@k1m31s08.42istanbul.com.tr>
2022-08-09 15:29:25 +0300

    42

--- mygnl/get_next_line.h
+++ mygnl/get_next_line.h
@@ -16,6 +16,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
